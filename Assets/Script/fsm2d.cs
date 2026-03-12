3219c368c1e0519b72720cd78f5d8adcf1b2668c
jajajagung <afrizajein@gmail.com>
2026-03-12 20:50:26 +0700

    Upload awal project FSM 2D Unity

---
+++ Assets/Script/fsm2d.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class fsm2d : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Chase,
+        Attack
+    }
+
+    public State currentState;
+    public Transform player;
+    public float chaseDistance = 3f;
+    public float speed = 2f;
+    public float attackDistance = 2f;
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        switch (currentState)
+        {
+            case State.Idle:
+
+                if (distance < chaseDistance)
+                {
+                    currentState = State.Chase;
+                }
+
+                break;
+
+            case State.Chase:
+
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+                if (distance < attackDistance)
+                {
+                    currentState = State.Attack;
+                }
+
+                break;
+
+            case State.Attack:
+
+                Debug.Log("Enemy Attack On");
+
+                if (distance > attackDistance)
+                {
+                    currentState = State.Chase;
+                }
+
+                break;
+        }
+    }
+}
